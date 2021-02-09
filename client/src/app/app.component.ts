710094c09ce81983f3ab1584b2a54e662438066e
K_van93 <kevin.vanaerschodt@gmail.com>
2021-02-09 16:25:22 +0100

    http --> https API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Social Media App';
+  users: any;
+  constructor(private _userService:UsersService) {
+
+  }
+
+  ngOnInit(){
+   this.getUsers()
+  }
+
+  getUsers(){
+    this._userService.getUsers().subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.error(error);
+    })
+  }
+
+
 }
--- client/src/app/users.service.ts
+++ client/src/app/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ export class UsersService implements OnInit{
   constructor(private http:HttpClient) {}
 
   ngOnInit() {
+  }
 
+  getUsers() : Observable<any>{
+    return this.http.get(`https://localhost:5001/api/users`)
   }
 }
