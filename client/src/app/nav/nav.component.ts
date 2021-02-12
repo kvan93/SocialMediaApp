8251c4cf7a8894b81507816bd11328c69b9ef24a
K_van93 <kevin.vanaerschodt@gmail.com>
2021-02-12 17:30:42 +0100

    Adding Routing guard + shared module + new style

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit {
   model: any = {};
 
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService, private router:Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -21,15 +23,16 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe( data => {
-      console.log(data);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout(){
     this.accountService.logout();
-
+    this.router.navigateByUrl('/');
   }
 
 
